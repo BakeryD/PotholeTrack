56235ffd8dd72c0762ec41080b7af107bd1ebb89
Justin Orgeron <Chebasaz@yahoo>
2018-08-13 15:02:02 -0400

    Index, map, and assorted

--- WebApplication.Web/wwwroot/js/site.js
+++ WebApplication.Web/wwwroot/js/site.js
@@ -7,3 +7,45 @@ $(document).ready(function () {
         $("#myModal").modal();
     });
 });
+
+
+function saveData() {
+    var submitter = placeholder('submitter');
+    var datecreated = placeholder('datecreated');
+    var dateinspected = placeholder('dateinspected');
+    var severity = placeholder('severity');
+    var repairdate = placeholder('repairdate');
+    var status = placeholder('status');
+    var reportcount = placeholder('reportcount');
+    var description = placeholder('description');
+    var latlng = marker.getPosition();
+    var url = 'phpsqlinfo_addrow.php?submitter=' + submitter + '&datecreated=' + datecreated + '&dateinspected=' + dateinspected + '&severity=' + severity + '&repairdate=' + repairdate + '&status=' + status + '&reportcount=' + reportcount + '&description=' + description + '&lattitude=' + latlng.lat() + '&longitude=' + latlng.lng();
+
+    downloadUrl(url, function (data, responseCode) {
+
+        if (responseCode == 200 && data.length <= 1) {
+            infowindow.close();
+            messagewindow.open(map, marker);
+        }
+    });
+}
+
+function downloadUrl(url, callback) {
+    var request = window.ActiveXObject ?
+        new ActiveXObject('Microsoft.XMLHTTP') :
+        new XMLHttpRequest;
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            request.onreadystatechange = doNothing;
+            callback(request.responseText, request.status);
+        }
+    };
+
+    request.open('GET', url, true);
+    request.send(null);
+}
+
+function doNothing() {
+};
+
