bbe1f608a9fac4cd65dc58e6f003f7d7e3e3f7ac
Andrew McCarthy <andrewmcc1120@gmail.com>
2018-08-14 11:53:46 -0400

    working on profile view

--- WebApplication.Web/Controllers/AccountController.cs
+++ WebApplication.Web/Controllers/AccountController.cs
@@ -41,25 +41,6 @@ namespace WebApplication.Web.Controllers
             return View(loginViewModel);
         }
 
-        [HttpPost]
-        [AuthorizationFilter("employee")]
-        [ValidateAntiForgeryToken]
-        public IActionResult Login(LoginViewModel loginViewModel)
-        {
-            // Ensure the fields were filled out
-            if (ModelState.IsValid)
-            {
-                // Check that they provided correct credentials
-                bool validLogin = authProvider.SignIn(loginViewModel.Username, loginViewModel.Password);
-                if (validLogin)
-                {
-                    // Redirect the user where you want them to go after successful login
-                    return RedirectToAction("Index", "Home");
-                }
-            }
-
-            return View(loginViewModel);
-        }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
